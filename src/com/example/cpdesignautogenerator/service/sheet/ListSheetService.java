4160fe3e022d4ed760162cbf5e10e9c9c5c73f0d
Kota Nomura <nomura.kota2005@gmail.com>
2025-08-17 23:58:20 +0900

    [add]ヘッダー作成まで完了

--- src/com/example/cpdesignautogenerator/service/sheet/ListSheetService.java
+++ src/com/example/cpdesignautogenerator/service/sheet/ListSheetService.java
@@ -1,11 +1,13 @@
 package com.example.cpdesignautogenerator.service.sheet;
 
+import org.apache.poi.ss.usermodel.BorderStyle;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.ss.util.RegionUtil;
 
 import com.example.cpdesignautogenerator.style.CellStyleController;
 
@@ -17,20 +19,57 @@ public class ListSheetService {
 
 		// セル幅調整
 		CellStyleController.setHeightAndWidth(sheet);
-
+		
+		// ----- 処理詳細 -----
 		// 1行目(A1) を作成して値を入れる
-		Row row = sheet.createRow(0);
-		Cell cell = row.createCell(0);
-		cell.setCellValue("処理詳細");
+		Row row1 = sheet.createRow(0);
+		Cell cellA1 = row1.createCell(0);
+		cellA1.setCellValue("処理詳細");
 
-		CellRangeAddress region = new CellRangeAddress(0, 1, 0, 5); // A1〜F2
-		sheet.addMergedRegion(region);
+		CellRangeAddress regionA1F1 = new CellRangeAddress(0, 1, 0, 5); // A1〜F2
+		sheet.addMergedRegion(regionA1F1);
 
 		// スタイル適応
-		CellStyle cellStyle = workbook.createCellStyle();
-		CellStyleController.setCellStyle(cellStyle);
-		CellStyleController.setLightBlueStyle(cellStyle);
-		CellStyleController.setRegionBorderStyle(sheet, region);
-		cell.setCellStyle(cellStyle);
+		CellStyle cellA1Style = workbook.createCellStyle();
+		CellStyleController.setCellStyle(cellA1Style);
+		CellStyleController.setLightBlueStyle(cellA1Style);
+		
+		cellA1.setCellStyle(cellA1Style);
+
+		// ----- モジュール名 -----
+		Cell cellG1 = row1.createCell(6);
+		cellG1.setCellValue("モジュール名");
+
+		CellRangeAddress regionG1AL1 = new CellRangeAddress(0, 0, 6, 37); // G1〜AL1
+		sheet.addMergedRegion(regionG1AL1);
+
+		// スタイル適応
+		CellStyle celG1Style = workbook.createCellStyle();
+		CellStyleController.setCellStyle(celG1Style);
+		CellStyleController.setLightBlueStyle(celG1Style);
+		cellG1.setCellStyle(celG1Style);
+		
+		Row row2 = sheet.createRow(1);
+		Cell cellG2 = row2.createCell(6);
+		cellG2.setCellValue("テストモジュール");
+
+		CellRangeAddress regionG2AL2 = new CellRangeAddress(1, 1, 6, 37); // G2〜AL2
+		sheet.addMergedRegion(regionG2AL2);
+
+		// スタイル適応
+		CellStyle celG2Style = workbook.createCellStyle();
+		CellStyleController.setCellStyle(celG2Style);
+		cellG2.setCellStyle(celG2Style);
+		
+		// ----- 枠線作成 -----
+		RegionUtil.setBorderRight(BorderStyle.THIN, regionA1F1, sheet);
+		
+		RegionUtil.setBorderBottom(BorderStyle.THIN, regionG1AL1, sheet);
+		
+		CellRangeAddress regionA1AL2 = new CellRangeAddress(0, 1, 0, 37); // A1〜AL2
+		RegionUtil.setBorderTop(BorderStyle.THIN, regionA1AL2, sheet);
+		RegionUtil.setBorderBottom(BorderStyle.THIN, regionA1AL2, sheet);
+		RegionUtil.setBorderLeft(BorderStyle.THIN, regionA1AL2, sheet);
+		RegionUtil.setBorderRight(BorderStyle.THIN, regionA1AL2, sheet);
 	}
 }
